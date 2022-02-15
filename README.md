8fea94d749652fd114abff3ddd7f74c7406d4108
Thomas Weber <muffin@muffin.ink>
2022-02-14 20:55:12 -0600

    Update to 1.1.1

---
+++ README.md
@@ -0,0 +1,7 @@
+To update:
+
+```
+python3 flatpak-node-generator.py npm --xdg-layout package-lock.json
+python3 update-library.py
+python3 update-packager.py
+```
---
+++ update-packager.py
@@ -0,0 +1,25 @@
+import json
+import urllib.request
+import aiohttp
+import asyncio
+import hashlib
+
+def fetch_json(url):
+  print(f'Fetching {url}')
+  with urllib.request.urlopen(url) as response:
+    contents = response.read()
+    return json.loads(contents)
+
+def get_packager():
+  packager = fetch_json('https://raw.githubusercontent.com/TurboWarp/desktop/master/scripts/packager.json')
+  return {
+    'type': 'file',
+    'url': packager['src'],
+    'sha256': packager['sha256'],
+    'dest': 'uncompressed',
+    'dest-filename': 'packager.html'
+  }
+
+sources = [get_packager()]
+with open('packager-sources.json', 'w') as f:
+  f.write(json.dumps(sources, indent=4))
