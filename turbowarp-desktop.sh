9820ed7a26c025a5cc37ec00f209aa387fb34420
Muffin <muffin@mailbox.org>
2024-04-19 14:26:11 -0500

    clean up

--- turbowarp-desktop.sh
+++ turbowarp-desktop.sh
@@ -2,10 +2,12 @@
 
 # Enable Wayland if it is available
 # From https://github.com/flathub/com.slack.Slack/blob/62e3c2d2236f2e8375c2bdd9c4a89158a004774a/slack.sh
+FLAGS=""
 WAYLAND_SOCKET=${WAYLAND_DISPLAY:-"wayland-0"}
 if [[ -e "$XDG_RUNTIME_DIR/${WAYLAND_SOCKET}" || -e "${WAYLAND_DISPLAY}" ]]
 then
-    FLAGS="--enable-features=WaylandWindowDecorations --ozone-platform-hint=auto"
+    echo "org.turbowarp.TurboWarp: enabling wayland"
+    FLAGS="--ozone-platform-hint=auto"
 fi
 
 exec zypak-wrapper /app/turbowarp/turbowarp-desktop $FLAGS "$@"
