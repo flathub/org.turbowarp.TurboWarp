635c76fe74db149739c0fe4d5411542ccf644319
Thomas Weber <muffin@muffin.ink>
2025-10-19 15:12:26 -0500

    Update to 1.15.0 (#42)

--- turbowarp-desktop.sh
+++ turbowarp-desktop.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Enable Wayland if it is available
-# From https://github.com/flathub/com.slack.Slack/blob/62e3c2d2236f2e8375c2bdd9c4a89158a004774a/slack.sh
-FLAGS=""
-WAYLAND_SOCKET=${WAYLAND_DISPLAY:-"wayland-0"}
-if [[ -e "$XDG_RUNTIME_DIR/${WAYLAND_SOCKET}" || -e "${WAYLAND_DISPLAY}" ]]
-then
-    echo "org.turbowarp.TurboWarp: enabling wayland"
-    FLAGS="--ozone-platform-hint=auto"
-fi
-
 # Disable the in-app update checker as updates are managed by flatpak.
 export TW_DISABLE_UPDATE_CHECKER=1
 
